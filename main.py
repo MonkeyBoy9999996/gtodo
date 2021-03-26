65dcaff3ed69fd7064cef449fb89ee138a11e1db
MonkeyBoy9999996 <samueljanmucha1@gmail.com>
2021-03-26 12:46:05 +0100

    Created base class containers for notes.
    Added todo and todos classes to main.py. These classes provides data handlers for data storing.
    -todo class contains initials base data. Priority and text attributes.
    -todos class contains list of todo entries sorted by priority.

--- main.py
+++ main.py
@@ -3,6 +3,44 @@
 import click
 
 
+class todo:
+    def __init__(self, priority, text):
+        self.priority = priority
+        self.text = text
+
+    def __lt__(self, other):
+        if self.priority < other.priority:
+            return True
+        else:
+            return False
+
+
+class todos:
+    def __init__(self):
+        self.plan = []
+
+    def list(self):
+        for i, t in enumerate(self.plan, start=1):
+            click.echo("[{}]: ".format(i), nl=False)
+            if t.priority == 1:
+                click.secho(t.text, fg='red')
+            elif t.priority == 2:
+                click.secho(t.text, fg='yellow')
+            elif t.priority == 3:
+                click.secho(t.text)
+            elif t.priority == 4:
+                click.secho(t.text, fg='blue')
+            else:
+                click.secho(t.text, fg='green')
+
+    def add(self, todo):
+        self.plan.append(todo)
+        self.plan.sort()
+
+    def delete(self, index):
+        self.plan.pop(index - 1)
+
+
 @click.group()
 def pytodo():
     '''CLI tool for TODO keeping'''
