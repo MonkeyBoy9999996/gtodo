33424ec6c4c5853dbfbbda9fa91a424e6e24ab43
MonkeyBoy9999996 <samueljanmucha1@gmail.com>
2021-03-28 10:19:52 +0200

    Updated setup.py with url of github.

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name="gtodo",
-      version='0.2',
+      version='0.2.1',
       description='Simple note taking CLI app',
       long_description=open('README.md').read(),
       author='Samuel Ján Mucha',
       author_email='samueljanmucha1@gmail.com',
+      url='https://github.com/MonkeyBoy9999996/gtodo',
       license='MIT',
       py_modules=['main'],
       install_requires=['Click', ],
